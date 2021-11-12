820e5311ee88dcd49cee714ed5c5d1e31bdc10c5
Yoon Jae <ys738@cornell.edu>
2021-11-12 18:53:29 +0900

    added static file and gave path to static file

--- recommendations/views.py
+++ recommendations/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.shortcuts import render
 # Create your views here.
 
 def homePageView(request):
-    return HttpResponse('Hello, Everyone!')
+    return render(request,'home.html')
